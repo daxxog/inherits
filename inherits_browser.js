07c46bb52d2fb19632e9d46648fc91db94ede508
David Volm <david@volminator.com>
2014-12-05 17:32:24 -0600

    UMD

--- inherits_browser.js
+++ inherits_browser.js
@@ -1,23 +1,42 @@
-if (typeof Object.create === 'function') {
-  // implementation from standard node.js 'util' module
-  module.exports = function inherits(ctor, superCtor) {
-    ctor.super_ = superCtor
-    ctor.prototype = Object.create(superCtor.prototype, {
-      constructor: {
-        value: ctor,
-        enumerable: false,
-        writable: true,
-        configurable: true
-      }
-    });
-  };
-} else {
-  // old school shim for old browsers
-  module.exports = function inherits(ctor, superCtor) {
-    ctor.super_ = superCtor
-    var TempCtor = function () {}
-    TempCtor.prototype = superCtor.prototype
-    ctor.prototype = new TempCtor()
-    ctor.prototype.constructor = ctor
+/* UMD LOADER: https://github.com/umdjs/umd/blob/master/returnExports.js */
+(function (root, factory) {
+    if (typeof exports === 'object') {
+        // Node. Does not work with strict CommonJS, but
+        // only CommonJS-like enviroments that support module.exports,
+        // like Node.
+        module.exports = factory();
+    } else if (typeof define === 'function' && define.amd) {
+        // AMD. Register as an anonymous module.
+        define(factory);
+    } else {
+        // Browser globals (root is window)
+        root.inherits = factory();
   }
-}
+}(this, function() {
+    if(typeof Object.create === 'function') {
+        // implementation from standard node.js 'util' module
+        inherits = function(ctor, superCtor) {
+            ctor.super_ = superCtor;
+
+            ctor.prototype = Object.create(superCtor.prototype, {
+                constructor: {
+                    value: ctor,
+                    enumerable: false,
+                    writable: true,
+                    configurable: true
+                }
+            });
+        };
+    } else {
+        // old school shim for old browsers
+        inherits = function(ctor, superCtor) {
+            ctor.super_ = superCtor;
+            var TempCtor = function () {};
+            TempCtor.prototype = superCtor.prototype;
+            ctor.prototype = new TempCtor();
+            ctor.prototype.constructor = ctor;
+        }
+    }
+
+    return inherits;
+}));
